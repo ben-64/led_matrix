e26a39c55afa29b8b380b65579d54388b8d826a2
ben64 <ben64@time0ut.org>
2020-04-16 11:27:08 +0200

    Possibility to concatenate images

--- led_matrix/image.py
+++ led_matrix/image.py
@@ -21,6 +21,40 @@ class Image(object):
     def from_coord(self,x,y):
         return y*self.width+x
 
+    def concatenate(self,img,empty_val=0,space=1):
+        data = []
+        diff = abs(self.height - img.height)
+        if img.height > self.height:
+            for j in range(0,diff):
+                for i in range(self.width+space):
+                    data.append(empty_val)
+                for i in range(img.width):
+                    data.append(img[i,j])
+            for j in range(self.height):
+                for i in range(self.width):
+                    data.append(self[i,j])
+                for i in range(space):
+                    data.append(empty_val)
+                for i in range(img.width):
+                    data.append(img[i,j+diff])
+        else:
+            for j in range(0,diff):
+                for i in range(self.width):
+                    data.append(self[i,j])
+                for i in range(space):
+                    data.append(empty_val)
+                for i in range(img.width):
+                    data.append(empty_val)
+            for j in range(img.height):
+                for i in range(self.width):
+                    data.append(self[i,j+diff])
+                for i in range(space):
+                    data.append(empty_val)
+                for i in range(img.width):
+                    data.append(img[i,j])
+        return Image(data,self.width+img.width+space,max(self.height,img.height))
+ 
+
 class SquareImage(Image):
     def __init__(self,data):
         side = int(math.sqrt(len(img)))
